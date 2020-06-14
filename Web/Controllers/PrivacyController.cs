c4eaaf1cc09bf27e7f259f5a740d7de60560d0c3
Bragde <47227297+Bragde@users.noreply.github.com>
2020-06-14 23:59:54 +0200

    Cookie policy

--- Web/Controllers/PrivacyController.cs
+++ Web/Controllers/PrivacyController.cs
@@ -1,26 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Web.Models;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Web.Controllers
 {
     public class PrivacyController : Controller
     {
-        public IActionResult Privacy()
+        public async Task<IActionResult> Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
